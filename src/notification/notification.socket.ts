663a12ee0a1b857a099422dfbc5d1a149021e838
bipin7giri <bipingiri27@gmail.com>
2024-04-19 20:38:26 +0545

    test

--- src/notification/notification.socket.ts
+++ src/notification/notification.socket.ts
@@ -2,6 +2,7 @@ import { type Socket } from "socket.io";
 
 export const NotificationSocketService = (io: any, socket: Socket): any => {
   socket.on("send_notification", (data) => {
+    console.log("hi");
     io.to(data.roomId).emit("receive_notification", {
       notification: "testing notifications",
     });
